a6cc22f39ab2455647ffafa67775799b48e31cf2
xrysamuel <xry200403@163.com>
2022-10-04 00:12:23 +0800

    221004-Change-1

--- live2d_widget/waifu-tips.js
+++ live2d_widget/waifu-tips.js
@@ -113,7 +113,7 @@ function loadWidget(config) {
 		fetch("https://v1.hitokoto.cn")
 			.then(response => response.json())
 			.then(result => {
-				const text = `这句一言来自 <span>「${result.from}」</span>，是 <span>${result.creator}</span> 在 hitokoto.cn 投稿的。`;
+				const text = `这句话来自 <span>「${result.from}」</span>，是 <span>${result.creator}</span> 在 hitokoto.cn 投稿的。`;
 				showMessage(result.hitokoto, 6000, 9);
 				setTimeout(() => {
 					showMessage(text, 4000, 9);
